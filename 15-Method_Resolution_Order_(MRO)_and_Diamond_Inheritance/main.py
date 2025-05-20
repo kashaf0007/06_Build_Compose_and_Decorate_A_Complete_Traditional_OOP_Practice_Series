39504148a8b83df0d85f1210db78b6af667a7b67
Kashaf Aman <166901772+kashaf0007@users.noreply.github.com>
2025-05-21 02:16:44 +0500

    Add files via upload

---
+++ 15-Method_Resolution_Order_(MRO)_and_Diamond_Inheritance/main.py
@@ -0,0 +1,16 @@
+class A:
+    def show(self):
+        print("Class A")
+class B(A):
+    def show(self):
+        print("Class B")
+class C(A):
+    def show(self):
+        print("Class C")
+class D (B,C):
+    pass
+
+obj = D()
+obj.show()
+
+print("MRO of class D:", D.__mro__)
